63e3012db3769ff75963439335b2b350646357d7
Mahesh Pareek <pareekm644@gmail.com>
2024-01-28 23:37:44 -0500

    Lab 2 Assignment

--- Lab2/ViewController.swift
+++ Lab2/ViewController.swift
@@ -9,6 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFirst: UITextField!
+    @IBOutlet weak var textLast: UITextField!
+    @IBOutlet weak var textCountry: UITextField!
+    @IBOutlet weak var textAge: UITextField!
+    @IBOutlet weak var textField: UITextView!
+    @IBOutlet weak var lblOutput: UILabel!
+    
+    
+    @IBAction func btnAdd(_ sender: UIButton) {
+        let txtFirst = textFirst.text
+        let txtLast = textLast.text
+        let txtCountry = textCountry.text
+        let txtAge = textAge.text
+     
+        if !(txtFirst == "" || txtLast == "" || txtAge == "" || txtCountry == "") {
+            textField.text = "Full Name: \(txtFirst ?? "") \(txtLast ?? "")\nCountry: \(txtCountry ?? "")\nAge: \(txtAge ?? "")"
+            
+            } else {
+                lblOutput.text = "Complete the missing Info!"
+            }
+    }
+    @IBAction func btnSubmit(_ sender: UIButton) {
+        lblOutput.text = "Successfully submitted!"
+        textFirst.text = ""
+        textLast.text = ""
+        textCountry.text = ""
+        textAge.text = ""
+        textField.text=""
+        
+    }
+    @IBAction func btnClear(_ sender: UIButton) {
+        textFirst.text = ""
+        textLast.text = ""
+        textCountry.text = ""
+        textAge.text = ""
+        textField.text = ""
+        lblOutput.text = ""
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
